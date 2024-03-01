f69ad21a4c8145d9c0cca915d4622c0870dd9423
Gourab Biswas <107937542+bgourab@users.noreply.github.com>
2024-03-01 07:44:41 +0530

    Time: 119 ms (90.15%), Space: 18.1 MB (54.05%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,10 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+        '''
         l=0
-        
-        
         for r in range(1,len(nums)):
             if nums[l]!=0:
                 l+=1
@@ -15,6 +13,13 @@ class Solution:
                 nums[r]=0
                 l+=1
             if nums[r]!=0:
-                r+=1
+                r+=1 
+        return''' 
+        
+        l=0
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[l],nums[r]=nums[r],nums[l]
+                l+=1
+        return nums
             
-        return 
